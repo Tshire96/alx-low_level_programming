3672af266008b6bb7c224db8f9ad9b05f355c89b
Tshire96 <maokatshireletso@gmail.com>
2023-03-15 13:45:51 -0700

    Write a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,79 +1,51 @@
 #include "main.h"
-
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-* str_concat - get ends of input and add together for size
-* @s1: input one to concat
-* @s2: input two to concat
-* Return: concat of s1 and s2
+* str_concat - Concatenates two strings of any size
+* @s1: the first string to concatenate
+* @s2: the second string to concatenate
+*
+* Return: the two strings concatenated
 */
-
 char *str_concat(char *s1, char *s2)
-
 {
-
-char *conct;
-
-int i, ci;
-
-
+int i = 0, j = 0, k = 0, l = 0;
+char *s;
 
 if (s1 == NULL)
-
 s1 = "";
 
 if (s2 == NULL)
-
 s2 = "";
 
-
-
-i = ci = 0;
-
-while (s1[i] != '\0')
-
+while (s1[i])
 i++;
 
-while (s2[ci] != '\0')
+while (s2[j])
+j++;
 
-ci++;
+l = i + j;
+s = malloc((sizeof(char) * l) +1);
 
-conct = malloc(sizeof(char) * (i + ci + 1));
-
-
-
-if (conct == NULL)
-
+if (s == NULL)
 return (NULL);
 
-i = ci = 0;
-
-while (s1[i] != '\0')
-
+j = 0;
+while (k < l)
 {
+if (k <= i)
+s[k] = s1[k];
 
-conct[i] = s1[i];
-
-i++;
-
-}
-
-
-
-while (s2[ci] != '\0')
-
+if (k >= i)
 {
-
-conct[i] = s2[ci];
-
-i++, ci++;
-
+s[k] = s2[j];
+j++;
 }
 
-conct[i] = '\0';
-
-return (conct);
-
+k++;
 }
-
+s[k] = '\0';
+return (s);
+}
